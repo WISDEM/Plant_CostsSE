daf50e1aa9c76b71d0a5d5084693499d20a6eca1
Andrew Ning <andrew.ning@nrel.gov>
2014-01-30 15:38:00 -0700

    removed some print execute statements

--- src/Plant_CostsSE/Plant_BOS/NREL_CSM_BOS/nrel_csm_bos.py
+++ src/Plant_CostsSE/Plant_BOS/NREL_CSM_BOS/nrel_csm_bos.py
@@ -22,7 +22,7 @@ class bos_csm_assembly(ExtendedBOSCostModel):
     def configure(self):
 
         super(bos_csm_assembly, self).configure()
-    
+
         self.replace('bos', bos_csm_component())
 
         self.connect('sea_depth','bos.sea_depth')
@@ -39,7 +39,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
     def __init__(self):
         """
         OpenMDAO component to wrap BOS model of the NREL _cost and Scaling Model (csmBOS.py)
-        
+
         Parameters
         ----------
         rotor_diameter : float
@@ -58,14 +58,14 @@ class bos_csm_component(ExtendedBOSCostAggregator):
           month of project start
         turbine_number : int
           number of turbines at plant
-          
+
         Returns
         -------
         BOS_cost : float
           Balance of station total costs excluding foundation
         plantBOS : PlantBOS
           Variable tree container for detailed BOS cost breakdown based on NREL Offshore _cost Breakdown Structure
-        
+
         """
         super(bos_csm_component, self).__init__()
 
@@ -76,11 +76,11 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         """
         Executes BOS model of the NREL _cost and Scaling Model to estimate wind plant BOS costs.
         """
-        
-        print "In {0}.execute()...".format(self.__class__)
 
-        lPrmtsCostCoeff1 = 9.94E-04 
-        lPrmtsCostCoeff2 = 20.31 
+        # print "In {0}.execute()...".format(self.__class__)
+
+        lPrmtsCostCoeff1 = 9.94E-04
+        lPrmtsCostCoeff2 = 20.31
         oPrmtsCostFactor = 37.0 # $/kW (2003)
         scourCostFactor =  55.0 # $/kW (2003)
         ptstgCostFactor =  20.0 # $/kW (2003)
@@ -91,7 +91,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         osInstallFactor  = 100.0 # $/kW (2003) shallow & trans
         suppInstallFactor = 330.0 # $/kW (2003) trans additional
         paiCost         = 60000.0 # per turbine
-        
+
         suretyBRate     = 0.03  # 3% of ICC
         suretyBond      = 0.0
 
@@ -107,7 +107,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
 
         # initialize self.ppi index calculator
         if iDepth == 1:
-            ref_yr  = 2002                   
+            ref_yr  = 2002
             ref_mon =    9
         else:
             ref_yr = 2003
@@ -144,12 +144,12 @@ class bos_csm_component(ExtendedBOSCostAggregator):
             fndnCostEscalator = 1.0
 
         foundation_cost *= fndnCostEscalator
-        
+
         # cost calculations
         tpC1  =0.00001581
         tpC2  =-0.0375
         tpInt =54.7
-        tFact = tpC1*self.machine_rating*self.machine_rating + tpC2*self.machine_rating + tpInt   
+        tFact = tpC1*self.machine_rating*self.machine_rating + tpC2*self.machine_rating + tpInt
 
         roadsCivil_costs = 0.0
         portStaging_costs = 0.0
@@ -167,10 +167,10 @@ class bos_csm_component(ExtendedBOSCostAggregator):
             engPermits_costs  = (lPrmtsCostCoeff1 * self.machine_rating * self.machine_rating) + \
                                (lPrmtsCostCoeff2 * self.machine_rating)
             ppi.ref_mon = 3
-            engPermits_costs *= ppi.compute('IPPI_LPM') 
+            engPermits_costs *= ppi.compute('IPPI_LPM')
             self.d_development_d_rating = ppi.compute('IPPI_LPM') * (2.0 * lPrmtsCostCoeff1 * self.machine_rating + lPrmtsCostCoeff2)
             ppi.ref_mon = 9
-            
+
             elC1  = 3.49E-06
             elC2  = -0.0221
             elInt = 109.7
@@ -178,7 +178,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
             electrical_costs = self.machine_rating * eFact * ppi.compute('IPPI_LEL')
             self.d_electrical_d_rating = ppi.compute('IPPI_LEL') * (3. * elC1*self.machine_rating**2. + \
                                     2. * elC2*self.machine_rating + elInt)
-            
+
             rcC1  = 2.17E-06
             rcC2  = -0.0145
             rcInt =69.54
@@ -186,13 +186,13 @@ class bos_csm_component(ExtendedBOSCostAggregator):
             roadsCivil_costs = self.machine_rating * rFact * ppi.compute('IPPI_RDC')
             self.d_preparation_d_rating = ppi.compute('IPPI_RDC') * (3. * rcC1 * self.machine_rating**2. + \
                                      2. * rcC2 * self.machine_rating + rcInt)
-             
+
             iCoeff = 1.965
             iExp   = 1.1736
             installation_costs = iCoeff * ((self.hub_height*self.rotor_diameter)**iExp) * ppi.compute('IPPI_LAI')
             self.d_assembly_d_diameter = iCoeff * ((self.hub_height*self.rotor_diameter)**(iExp-1)) * self.hub_height * ppi.compute('IPPI_LAI')
             self.d_assembly_d_hheight = iCoeff * ((self.hub_height*self.rotor_diameter)**(iExp-1)) * self.rotor_diameter * ppi.compute('IPPI_LAI')
-          
+
             transportation_costs = self.machine_rating * tFact * ppi.compute('IPPI_TPT')
             self.d_transport_d_rating = ppi.compute('IPPI_TPT') * (tpC1* 3. * self.machine_rating**2. + \
                                    tpC2* 2. * self.machine_rating + tpInt )
@@ -201,16 +201,16 @@ class bos_csm_component(ExtendedBOSCostAggregator):
             ppi.ref_yr = 2003
             pai_costs            = paiCost * ppi.compute('IPPI_PAE')
             portStaging_costs    = ptstgCostFactor  * self.machine_rating * ppi.compute('IPPI_STP') # 1.415538133
-            self.d_preparation_d_rating = ptstgCostFactor * ppi.compute('IPPI_STP') 
+            self.d_preparation_d_rating = ptstgCostFactor * ppi.compute('IPPI_STP')
             engPermits_costs     = oPrmtsCostFactor * self.machine_rating * ppi.compute('IPPI_OPM')
             self.d_development_d_rating = oPrmtsCostFactor * ppi.compute('IPPI_OPM')
             scour_costs         = scourCostFactor  * self.machine_rating * ppi.compute('IPPI_STP') # 1.415538133#
             self.d_other_d_rating = scourCostFactor  * ppi.compute('IPPI_STP')
-            installation_costs   = osInstallFactor  * self.machine_rating * ppi.compute('IPPI_OAI')            
+            installation_costs   = osInstallFactor  * self.machine_rating * ppi.compute('IPPI_OAI')
             self.d_assembly_d_rating = osInstallFactor * ppi.compute('IPPI_OAI')
             electrical_costs     = ossElCostFactor  * self.machine_rating * ppi.compute('IPPI_OEL')
             self.d_electrical_d_rating = ossElCostFactor  * ppi.compute('IPPI_OEL')
-            ppi.ref_yr  = 2002                   
+            ppi.ref_yr  = 2002
             transportation_costs = self.machine_rating * tFact * ppi.compute('IPPI_TPT')
             self.d_transport_d_rating = ppi.compute('IPPI_TPT') * (tpC1* 3. * self.machine_rating**2. + \
                                    tpC2* 2. * self.machine_rating + tpInt )
@@ -232,16 +232,16 @@ class bos_csm_component(ExtendedBOSCostAggregator):
             scour_costs          = scourCostFactor  * self.machine_rating * ppi.compute('IPPI_STP')
             self.d_other_d_rating = scourCostFactor * ppi.compute('IPPI_STP')
             ppi.ref_yr  = 2002
-            turbTrans           = ostTTransFactor  * self.machine_rating * ppi.compute('IPPI_TPT') 
+            turbTrans           = ostTTransFactor  * self.machine_rating * ppi.compute('IPPI_TPT')
             self.d_transport_d_rating = ostTTransFactor  * ppi.compute('IPPI_TPT')
             ppi.ref_yr = 2003
-            supportTrans        = ostSTransFactor  * self.machine_rating * ppi.compute('IPPI_OAI') 
+            supportTrans        = ostSTransFactor  * self.machine_rating * ppi.compute('IPPI_OAI')
             transportation_costs = self.turbTrans + self.supportTrans
-            self.d_transport_d_rating += ostSTransFactor  * ppi.compute('IPPI_OAI') 
-                
+            self.d_transport_d_rating += ostSTransFactor  * ppi.compute('IPPI_OAI')
+
         elif (iDepth == 4):  # offshore deep
             print "\ncsmBOS: Add costCat 4 code\n\n"
-       
+
         bos_costs = foundation_cost + \
                     transportation_costs + \
                     roadsCivil_costs    + \
@@ -259,7 +259,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
             d_surety_d_rating = suretyBRate * (self.d_development_d_rating + self.d_preparation_d_rating + self.d_transport_d_rating + \
                           self.d_foundation_d_rating + self.d_electrical_d_rating + self.d_assembly_d_rating + self.d_other_d_rating)
             self.d_other_d_rating += d_surety_d_rating
-        
+
         self.bos_costs = self.turbine_number * (bos_costs + suretyBond)
 
         self.BOS_breakdown.development_costs = engPermits_costs * self.turbine_number
@@ -270,7 +270,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         self.BOS_breakdown.assembly_and_installation_costs = installation_costs * self.turbine_number
         self.BOS_breakdown.soft_costs = 0.0
         self.BOS_breakdown.other_costs = (pai_costs + scour_costs + suretyBond) * self.turbine_number
-        
+
         '''print self.BOS_breakdown.foundation_and_substructure_costs
         print self.BOS_breakdown.transportation_costs
         print self.BOS_breakdown.preparation_and_staging_costs
@@ -293,7 +293,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         self.d_cost_d_rating = self.d_development_d_rating + self.d_preparation_d_rating + self.d_transport_d_rating + \
                           self.d_foundation_d_rating + self.d_electrical_d_rating + self.d_assembly_d_rating + \
                           self.d_soft_d_rating + self.d_other_d_rating
-                          
+
         self.d_development_d_diameter = 0.0
         self.d_preparation_d_diameter = 0.0
         self.d_transport_d_diameter = 0.0
@@ -304,7 +304,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         self.d_other_d_diameter = 0.0
         self.d_cost_d_diameter = self.d_development_d_diameter + self.d_preparation_d_diameter + self.d_transport_d_diameter + \
                           self.d_foundation_d_diameter + self.d_electrical_d_diameter + self.d_assembly_d_diameter + \
-                          self.d_soft_d_diameter + self.d_other_d_diameter       
+                          self.d_soft_d_diameter + self.d_other_d_diameter
 
         self.d_development_d_tcc = 0.0
         self.d_preparation_d_tcc = 0.0
@@ -321,7 +321,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         self.d_development_d_hheight = 0.0
         self.d_preparation_d_hheight = 0.0
         self.d_transport_d_hheight = 0.0
-        #self.d_foundation_d_hheight 
+        #self.d_foundation_d_hheight
         self.d_electrical_d_hheight = 0.0
         #self.d_assembly_d_hheight
         self.d_soft_d_hheight = 0.0
@@ -341,10 +341,10 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         self.d_cost_d_rna = self.d_development_d_rna + self.d_preparation_d_rna + self.d_transport_d_rna + \
                           self.d_foundation_d_rna + self.d_electrical_d_rna + self.d_assembly_d_rna + \
                           self.d_soft_d_rna + self.d_other_d_rna
-    
+
     def list_deriv_vars(self):
 
-        inputs = ['machine_rating', 'rotor_diameter', 'turbine_cost', 'hub_height', 'RNA_mass']     
+        inputs = ['machine_rating', 'rotor_diameter', 'turbine_cost', 'hub_height', 'RNA_mass']
         outputs = ['BOS_breakdown.development_costs', 'BOS_breakdown.preparation_and_staging_costs',\
                    'BOS_breakdown.transportation_costs', 'BOS_breakdown.foundation_and_substructure_costs',\
                    'BOS_breakdown.electrical_costs', 'BOS_breakdown.assembly_and_installation_costs',\
@@ -353,7 +353,7 @@ class bos_csm_component(ExtendedBOSCostAggregator):
         return inputs, outputs
 
     def provideJ(self):
-        
+
         self.J = np.array([[self.d_development_d_rating, self.d_development_d_diameter, self.d_development_d_tcc, self.d_development_d_hheight, self.d_development_d_rna],\
                            [self.d_preparation_d_rating, self.d_preparation_d_diameter, self.d_preparation_d_tcc, self.d_preparation_d_hheight, self.d_preparation_d_rna],\
                            [self.d_transport_d_rating, self.d_transport_d_diameter, self.d_transport_d_tcc, self.d_transport_d_hheight, self.d_transport_d_rna],\
@@ -363,26 +363,26 @@ class bos_csm_component(ExtendedBOSCostAggregator):
                            [self.d_soft_d_rating, self.d_soft_d_diameter, self.d_soft_d_tcc, self.d_soft_d_hheight, self.d_soft_d_rna],\
                            [self.d_other_d_rating, self.d_other_d_diameter, self.d_other_d_tcc, self.d_other_d_hheight, self.d_other_d_rna],\
                            [self.d_cost_d_rating, self.d_cost_d_diameter, self.d_cost_d_tcc, self.d_cost_d_hheight, self.d_cost_d_rna]])
-      
-        return self.J    
-   
+
+        return self.J
+
 #-----------------------------------------------------------------
 
 def example():
-  
+
     # simple test of module
     bos = bos_csm_assembly()
     bos.machine_rating = 5000.0
     bos.rotor_diameter = 126.0
     bos.turbine_cost = 5950209.283
-    bos.turbine_number = 100 
+    bos.turbine_number = 100
     bos.hub_height = 90.0
     bos.RNA_mass = 256634.5 # RNA mass is not used in this simple model
     bos.execute()
     print "BOS cost offshore: {0}".format(bos.bos_costs)
     print "BOS cost per turbine: {0}".format(bos.bos_costs / bos.turbine_number)
     print
-    
+
     bos.sea_depth = 0.0
     bos.turbine_cost = 5229222.77
     bos.execute()
--- src/Plant_CostsSE/Plant_OM/NREL_CSM_OM/nrel_csm_om.py
+++ src/Plant_CostsSE/Plant_OM/NREL_CSM_OM/nrel_csm_om.py
@@ -28,9 +28,9 @@ class om_csm_assembly(ExtendedOPEXModel):
     def configure(self):
 
         super(om_csm_assembly,self).configure()
-      
+
         self.replace('opex', om_csm_component())
-        
+
         self.connect('machine_rating','opex.machine_rating')
         self.connect('sea_depth','opex.sea_depth')
         self.connect('net_aep','opex.net_aep')
@@ -49,7 +49,7 @@ class om_csm_component(ExtendedOPEXAggregator):
     year = Int(2009, units='yr', iotype='in', desc='year for project start')
     month = Int(12, iotype = 'in', desc= 'month for project start') # units = months
     turbine_number = Int(100, iotype = 'in', desc = 'number of turbines at plant')
- 
+
     def __init__(self):
         """
         OpenMDAO component to wrap O&M model of the NREL _cost and Scaling model data (csmOM.py).
@@ -68,13 +68,13 @@ class om_csm_component(ExtendedOPEXAggregator):
           number of turbines at plant
         net_aep : float
           annual energy production for the plant [kWh]
-        
+
         Returns
         -------
         OnMcost : float
           O&M costs [USD]
         plantOM : PlantOM
-          Variable tree for detailed O&M outputs    
+          Variable tree for detailed O&M outputs
         """
         super(om_csm_component, self).__init__()
 
@@ -85,7 +85,7 @@ class om_csm_component(ExtendedOPEXAggregator):
         """
         Execute the O&M Model of the NREL _cost and Scaling Model.
         """
-        print "In {0}.execute()...".format(self.__class__)
+        # print "In {0}.execute()...".format(self.__class__)
 
         # initialize variables
         if self.sea_depth == 0:
@@ -93,36 +93,36 @@ class om_csm_component(ExtendedOPEXAggregator):
         else:
             offshore = True
         ppi.curr_yr = self.year
-        ppi.curr_mon = self.month  
+        ppi.curr_mon = self.month
 
-        #O&M     
+        #O&M
         offshoreCostFactor = 0.0200  # $/kwH
         landCostFactor     = 0.0070  # $/kwH
         if not offshore:  # kld - place for an error check - iShore should be in 1:4
             cost = self.net_aep * landCostFactor
-            costEscalator = ppi.compute('IPPI_LOM') 
+            costEscalator = ppi.compute('IPPI_LOM')
         else:
             cost = self.net_aep * offshoreCostFactor
             ppi.ref_yr = 2003
             costEscalator = ppi.compute('IPPI_OOM')
-            ppi.ref_yr = 2002 
+            ppi.ref_yr = 2002
 
         self.OPEX_breakdown.preventative_opex = cost * costEscalator # in $/year
 
-        #LRC        
-        if not offshore: 
+        #LRC
+        if not offshore:
             lrcCF = 10.70 # land based
             costlrcEscFactor = ppi.compute('IPPI_LLR')
         else: #TODO: transition and deep water options if applicable
             lrcCF = 17.00 # offshore
             ppi.ref_yr = 2003
             costlrcEscFactor = ppi.compute('IPPI_OLR')
-            ppi.ref_yr = 2002 
-                
+            ppi.ref_yr = 2002
+
         self.OPEX_breakdown.corrective_opex = self.machine_rating * lrcCF * costlrcEscFactor * self.turbine_number # in $/yr
 
         #LLC
-        if not offshore: 
+        if not offshore:
             leaseCF = 0.00108 # land based
             costlandEscFactor = ppi.compute('IPPI_LSE')
         else: #TODO: transition and deep water options if applicable
@@ -137,8 +137,8 @@ class om_csm_component(ExtendedOPEXAggregator):
         #Total OPEX
         self.avg_annual_opex = self.OPEX_breakdown.preventative_opex + self.OPEX_breakdown.corrective_opex \
            + self.OPEX_breakdown.lease_opex
-        
-        
+
+
         #dervivatives
         self.d_corrective_d_aep = 0.0
         self.d_corrective_d_rating = lrcCF * costlrcEscFactor * self.turbine_number
@@ -162,13 +162,13 @@ class om_csm_component(ExtendedOPEXAggregator):
                   'OPEX_breakdown.other_opex', 'avg_annual_opex']
 
         return inputs, outputs
-    
+
     def provideJ(self):
 
         self.J = np.array([[self.d_preventative_d_aep, self.d_preventative_d_rating], [self.d_corrective_d_aep, self.d_corrective_d_rating],\
                            [self.d_lease_d_aep, self.d_lease_d_rating], [self.d_other_d_aep, self.d_other_d_rating],\
                            [self.d_opex_d_aep, self.d_opex_d_rating]])
-        
+
         return self.J
 
 
@@ -177,7 +177,7 @@ def example():
     # simple test of module
 
     om = om_csm_assembly()
-    
+
     om.machine_rating = 5000.0 # Need to manipulate input or underlying component will not execute
     om.net_aep = 1701626526.28
     om.sea_depth = 20.0
