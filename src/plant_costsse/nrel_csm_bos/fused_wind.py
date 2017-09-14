a4a205211a82fecc67124d2b3d2d1ca38edcab2d
Justin Gray <justin.s.gray@gmail.com>
2017-09-14 19:13:56 +0200

    cleaned up the io processing ab it

--- src/plant_costsse/nrel_csm_bos/fused_wind.py
+++ src/plant_costsse/nrel_csm_bos/fused_wind.py
@@ -105,7 +105,12 @@ class FUSED_OpenMDAO(Component):
 
         self.model = model
 
-        for k, v in self.model.interface['input'].items():
+        self._process_io(self.model.interface['input'], self.add_param)
+        self._process_io(self.model.interface['output'], self.add_output)
+
+    def _process_io(self, interface, add_method):
+
+        for k, v in interface.items():
 
             # Apply the sizes of arrays
             if 'shape' in v.keys():
@@ -120,25 +125,8 @@ class FUSED_OpenMDAO(Component):
                     v['val']=np.zeros(v['shape'], dtype=float)
             else:
                 v['val'] = float(v['val'])
-            self.add_param(k, v['val'])
 
-        for k, v in self.model.interface['output'].items():
-
-            # Apply the sizes of arrays
-            if 'shape' in v.keys():
-                for i, sz in enumerate(v['shape']):
-                    if type(sz) is not int:
-                        my_name = sz['name']
-                        if my_name not in kwargs.keys():
-                            print('The interface requires that the size '+my_name+' is specified')
-                            raise Exception
-                        v['shape'][i]=kwargs[my_name]
-                if 'val' in v.keys():
-                    v['val']=np.zeros(v['shape'])
-
-            else:
-                v['val'] = float(v['val'])
-            self.add_output(k, v['val'])
+            add_method(k, v['val'])
 
     def solve_nonlinear(self, params, unknowns, resids):
 
