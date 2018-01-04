bec3a817b21c0b0a40c3b69773f2b4b67a50a342
Katherine Dykes <katherine.dykes@nrel.gov>
2018-01-04 15:46:19 -0700

    reintegrating the land_bos installation

--- setup.py
+++ setup.py
@@ -29,3 +29,31 @@ kwargs = {'author': '',
 
 setup(**kwargs)
 
+# set up for landbased bos model
+from distutils.core import setup
+from distutils.extension import Extension
+
+try:
+    USE_CYTHON = True
+    from Cython.Build import cythonize
+except Exception:
+    USE_CYTHON = False
+
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [Extension('_landbos', ['src/plant_costsse/nrel_land_bosse/_landbos'+ext, 'src/plant_costsse/nrel_land_bosse/LandBOSsmooth.c'])]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
+
+setup(
+    name='NREL_Land_BOSSE',
+    description='a translation of the NREL landbased balance of station excel model',
+    author='S. Andrew Ning',
+    author_email='andrew.ning@nrel.gov',
+    package_dir={'': 'src'},
+    py_modules=['plant_costsse.nrel_land_bosse.nrel_land_bosse'],
+    license='Apache License, Version 2.0',
+    ext_modules=extensions
+)
